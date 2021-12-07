2c74629ab9453fc3938a627598f52b14f81462d1
Brett Stedford <brett.stedford@ao.com>
2021-12-07 09:29:46 +0000

    Day 2 - Part 2

--- day2.py
+++ day2.py
@@ -5,18 +5,20 @@ lines: list[str] = get_input_as_lines('day2')
 
 x = 0
 y = 0
+aim = 0
 
 for (i, val) in enumerate(lines):  
-    axis = val.split(' ')[0]
+    command = val.split(' ')[0]
     velocity = int(val.split(' ')[1])
 
-    if(axis == 'down'):
-        y += velocity
+    if(command == 'down'):
+        aim += velocity
 
-    if(axis == 'up'):
-        y -= velocity
+    if(command == 'up'):
+        aim -= velocity
 
-    if(axis == 'forward'):
+    if(command == 'forward'):
         x += velocity
+        y += velocity*aim
 
 print(x * y)
