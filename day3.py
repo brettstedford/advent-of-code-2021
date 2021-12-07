a7a667a4e29f196568847aba5f1bc1fdb6a4acca
Brett Stedford <brett.stedford@ao.com>
2021-12-07 13:09:25 +0000

    Day 3 - Part 2

--- day3.py
+++ day3.py
@@ -1,35 +1,49 @@
 from common import get_input_as_lines
 
-lines: list[str] = get_input_as_lines('day3')
-
-gamma_binary = ''
-epsilon_binary = ''
-
-data_len = len(lines[0])
-
-for i in range(0, data_len):
+def get_signficant_bit(data:list[str], bit_pos: int, type: str='most'):
     high = 0
     low = 0
     
-    for j, val in enumerate(lines):
-        bit = int(list(val)[i])
+    for _, val in enumerate(data):
+        bit = int(list(val)[bit_pos])
         if(bit == 0):
             low+=1
         
         if(bit == 1):
             high+=1
 
-    if(high > low):
-        gamma_binary += '1'
-        epsilon_binary += '0'
+    if(high > low or high == low):
+        return 1 if type == "most" else 0
     
     if(low > high):
-        gamma_binary += '0'
-        epsilon_binary += '1'
+        return 0 if type == "most" else 1
 
-gamma_rate = int(gamma_binary, 2)
-epsilon_rate = int(epsilon_binary, 2)
+def filter_by_bit_at_pos(data:list[str], bit_pos:int, sig_bit:int):
+    f = filter(lambda bits: int(list(bits)[bit_pos]) == sig_bit, data)
+    return list(f)
 
-print(gamma_binary, '>>>', gamma_rate)
-print(epsilon_binary, '>>>', epsilon_rate)
-print(gamma_rate, '*', epsilon_rate, '>>>', gamma_rate * epsilon_rate)
+
+lines: list[str] = get_input_as_lines('day3')
+
+oxy_gen_rating: list[str] = lines
+c02_scrubber_rating: list[str] = lines
+
+data_len = len(lines[0])
+
+for i in range(0, data_len):
+    ogr_sig_bit = get_signficant_bit(oxy_gen_rating, i)
+    cs_sig_bit = get_signficant_bit(c02_scrubber_rating, i, 'least')
+    
+    print(i)
+    print('oxy', ogr_sig_bit)
+    print('c02', cs_sig_bit)
+
+    if(len(oxy_gen_rating) > 1):
+        oxy_gen_rating = filter_by_bit_at_pos(oxy_gen_rating, i, ogr_sig_bit)
+    
+    if(len(c02_scrubber_rating) > 1):
+        c02_scrubber_rating = filter_by_bit_at_pos(c02_scrubber_rating, i, cs_sig_bit)
+
+ls_rating = int(oxy_gen_rating[0], 2) * int(c02_scrubber_rating[0], 2)
+
+print(ls_rating)
